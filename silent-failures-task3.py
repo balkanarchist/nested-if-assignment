0e17a8e6afe434194a24f46cab442383acf7f5be
balkanarchist <balkanarchist@yandex.com>
2024-03-02 14:14:07 -0700

    added code to silent-failures-task3

---
+++ silent-failures-task3.py
@@ -0,0 +1,12 @@
+'''
+Ask the user for a filename to read. Try to open and
+read the file. If the file doesn't exist, use
+the pass statement to handle the error silently.
+'''
+file = input("Enter a filename to read: ")
+
+try:
+    file = open("")
+    pass
+except:
+    pass
